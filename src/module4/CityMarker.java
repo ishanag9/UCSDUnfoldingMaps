eb2c7505330177a212eff30a144e9c9ba417689f
Ishan Agrawal <ishan38@outlook.com>
2020-05-25 00:53:47 +0530

    added city markers

--- src/module4/CityMarker.java
+++ src/module4/CityMarker.java
@@ -9,7 +9,7 @@ import processing.core.PGraphics;
 /** Implements a visual marker for cities on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Ishan Agrawal
  *
  */
 public class CityMarker extends SimplePointMarker {
@@ -45,8 +45,9 @@ public class CityMarker extends SimplePointMarker {
 		// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
 		// whose upper left corner is at position x, y
 		// Check out the processing documentation for more methods
-		
-		
+		pg.fill(214, 54, 83);
+		pg.triangle(x - 5, y + 10, x, y, x + 5, y + 10);
+
 		// Restore previous drawing style
 		pg.popStyle();
 	}
