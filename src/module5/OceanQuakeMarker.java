674946c98ceb875a92f5cc27451c66379d111fc9
Ishan Agrawal <ishan38@outlook.com>
2020-05-25 02:08:12 +0530

    implements a visual marker for ocean earthquakes and if clicked, lines are drawn to all the cities that may be affected by it

--- src/module5/OceanQuakeMarker.java
+++ src/module5/OceanQuakeMarker.java
@@ -1,16 +1,21 @@
 package module5;
 
 import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.utils.ScreenPosition;
 import processing.core.PGraphics;
+
+import java.util.List;
 
 /** Implements a visual marker for ocean earthquakes on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Ishan Agrawal
  *
  */
 public class OceanQuakeMarker extends EarthquakeMarker {
-	
+
+	private List<ScreenPosition> affectedLocations;
+
 	public OceanQuakeMarker(PointFeature quake) {
 		super(quake);
 		
@@ -23,6 +28,14 @@ public class OceanQuakeMarker extends EarthquakeMarker {
 	@Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
 		pg.rect(x-radius, y-radius, 2*radius, 2*radius);
+
+		//If an ocean earthquake is clicked, lines are drawn to all the cities that may be affected by it
+		if(getClicked()){
+			affectedLocations = (List<ScreenPosition>) this.getProperty("affectedLocations");
+			for (ScreenPosition city : affectedLocations) {
+				pg.line(x, y, city.x, city.y);
+			}
+		}
 	}
 	
 
