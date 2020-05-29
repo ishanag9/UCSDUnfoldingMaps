abf85ce9cab80a239ed1e2c99b70f872bc2e37e1
Ishan Agrawal <ishan38@outlook.com>
2020-05-30 01:46:48 +0530

    implements Comparable

--- src/module6/EarthquakeMarker.java
+++ src/module6/EarthquakeMarker.java
@@ -6,11 +6,11 @@ import processing.core.PGraphics;
 
 /** Implements a visual marker for earthquakes on an earthquake map
  * 
- * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Ishan Agrawal
  *
  */
 // TODO: Implement the comparable interface
-public abstract class EarthquakeMarker extends CommonMarker
+public abstract class EarthquakeMarker extends CommonMarker implements Comparable<EarthquakeMarker>
 {
 	
 	// Did the earthquake occur on land?  This will be set by the subclasses.
@@ -54,10 +54,18 @@ public abstract class EarthquakeMarker extends CommonMarker
 		setProperties(properties);
 		this.radius = 1.75f*getMagnitude();
 	}
-	
-	// TODO: Add the method:
-	// public int compareTo(EarthquakeMarker marker)
-	
+
+
+	public int compareTo(EarthquakeMarker marker) {
+		if (this.getMagnitude() < marker.getMagnitude()) {
+			return 1;
+		} else if (this.getMagnitude() > marker.getMagnitude()) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
 	
 	// calls abstract method drawEarthquake and then checks age and draws X if needed
 	@Override
